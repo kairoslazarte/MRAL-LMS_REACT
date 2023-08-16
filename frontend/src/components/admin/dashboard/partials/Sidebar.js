79c497559acb4b0373f6a9685bfbf4dda3895cda
Lazarte <fillip.k.lazarte@accenture.com>
2023-08-17 00:46:13 +0800

    add logout btn in admin dashboard

--- frontend/src/components/admin/dashboard/partials/Sidebar.js
+++ frontend/src/components/admin/dashboard/partials/Sidebar.js
@@ -164,6 +164,21 @@ const Sidebar = () => {
                                     )}
                                 </div>
                             )}
+                            <div>
+                                <div className="flex items-center justify-between w-full text-gray-400">
+                                    <button
+                                        type="button"
+                                        className="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md w-full"
+                                        onClick={() => window.location.reload()}
+                                    >
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor"  
+                                        className="text-gray-400 group-hover:text-gray-300 mr-3 flex-shrink-0 h-6 w-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+                                        </svg>
+                                        Sign out
+                                    </button>
+                                </div>
+                            </div>
                         </nav>
                     </div>
                 </div>
