7b9a54e7dd253f950c7f2ebdbe3100131e374ef2
filliplazarteACG <fillip.lazarte@accessgroup.net.au>
2025-06-23 10:50:34 +0800

    update student default pass for SY 25-26

--- frontend/src/components/admin/dashboard/students/CreateStudentAccount.js
+++ frontend/src/components/admin/dashboard/students/CreateStudentAccount.js
@@ -180,7 +180,7 @@ const CreateStudentAccount = () => {
                                     </div> 
                                     <div className="mb-6">
                                         <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Auto generated password</label>
-                                        <input type="text" id="password" name="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={"mral2024"} value={"mral2024"} required disabled />
+                                        <input type="text" id="password" name="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={"mral2025"} value={"mral2025"} required disabled />
                                     </div> 
                                 </div>
                             </div>
